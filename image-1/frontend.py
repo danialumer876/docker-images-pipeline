1337174579750a2f90c06b513a95fdf0baa4156f
danialumer876 <82091264+danialumer876@users.noreply.github.com>
2023-01-14 13:57:37 +0500

    Update frontend.py

--- image-1/frontend.py
+++ image-1/frontend.py
@@ -11,7 +11,7 @@ def hello():
     response = requests.get('http://backend-service:5100/response')
     e = datetime.datetime.now()
     data = response.json()
-    return f"{e.day}/{e.month}/{e.year} {e.hour}:{e.minute}  Hello {data['name']}"
+    return f"{e.day}/{e.month}/{e.year}  Hello {data['name']}"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5200)
